73eec54febd8ea7449e73dd3fbf5f10e0a84a98f
Justin Bond <justin.d.bond@gmail.com>
2016-06-19 23:20:51 -0400

    settings is done

--- www/js/ptcontrollers.js
+++ www/js/ptcontrollers.js
@@ -1,16 +1,22 @@
 /*global angular*/
 var mod = angular.module('ptcontrollers', []);
 
-mod.controller('SettingsCtrl', function ($scope, $log, config) {
+mod.controller('SettingsCtrl', function ($scope, $state, $log, config) {
     "use strict";
     $log.info("Begin SettingsCtrl");
 
-    var levelToName;
-
     $scope.settings = { "level" : config.defaultLevel,
-                        "tables" : ["six"]
+                        "tables" : null
         };
 
+    $scope.$watch('settings.tables', function () {
+        $log.debug("tables changed: " + JSON.stringify($scope.settings.tables));
+    });
+
+    $scope.startDrill = function (settings) {
+        $log.debug("Starting drill with settings: ", JSON.stringify(settings));
+        $state.go('drill', {settings: settings});
+    };
 });
 
 mod.controller('DrillCtrl', function ($scope, $log) {
